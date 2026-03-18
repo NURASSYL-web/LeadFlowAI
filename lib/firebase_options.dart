c65fa67dc5a851a08d8c3f1db3dc56bba4a81a85
LeadFlow User <user@example.com>
2026-03-19 01:32:28 +0500

    Add Firebase web configuration

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,14 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart'
+    show TargetPlatform, defaultTargetPlatform, kIsWeb;
 
 class DefaultFirebaseOptions {
-  static const String _setupMessage =
-      'Firebase config is not bundled in this repository build yet.';
+  static const String _nonWebSetupMessage =
+      'Firebase config is not bundled for this platform in the repository build yet.';
 
-  static String? get currentPlatformConfigurationError => _setupMessage;
+  static String? get currentPlatformConfigurationError {
+    if (kIsWeb) {
+      return null;
+    }
+    return _nonWebSetupMessage;
+  }
 
   static FirebaseOptions get currentPlatform {
-    throw UnsupportedError(_setupMessage);
+    if (kIsWeb) {
+      return web;
+    }
+
+    throw UnsupportedError(_nonWebSetupMessage);
   }
 
   static FirebaseOptions placeholderForPlatform(TargetPlatform platform) {
@@ -19,4 +29,14 @@ class DefaultFirebaseOptions {
       projectId: 'placeholder',
     );
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBn7c3W_mZ3z3ztZdk4NNt0ZQd57Q2WGEM',
+    appId: '1:279689050864:web:565ead8645c5786c882035',
+    messagingSenderId: '279689050864',
+    projectId: 'leadflow-9d3b0',
+    authDomain: 'leadflow-9d3b0.firebaseapp.com',
+    storageBucket: 'leadflow-9d3b0.firebasestorage.app',
+    measurementId: 'G-F3CYPD8XZH',
+  );
 }
